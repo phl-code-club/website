f94861398cefc19f5dd77f021547c5a2dcf63960
Your Name~ <tcummings615@gmail.com>
2026-01-05 17:58:39 -0500

    feat: initial blog setup

---
+++ src/blog/post1.md
@@ -0,0 +1,11 @@
+---
+title: October Meetup
+summary: short summary
+pubDate: 2026-01-05
+author: Taj
+---
+# October Meetup
+
+We had a bunch of fun!
+
+![group photo](../assets/events/october-2026/Group.jpeg)
---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+
+// 1. Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+// 4. Define your collection(s)
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+    schema: z.object({
+        title: z.string(),
+        summary: z.string(),
+        pubDate: z.coerce.date(),
+        author: z.string(),
+    })
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
